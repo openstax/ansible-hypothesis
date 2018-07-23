e6fe0c2d04dd96118f6d5746556c32ed2b964e7c
Thomas Woodward <tom.g.woodward@gmail.com>
2018-07-23 11:50:28 -0400

    add user role to research export

--- files/annotation_export.sql
+++ files/annotation_export.sql
@@ -5,12 +5,13 @@ SELECT
 , updated AS updated_on
 , CASE WHEN deleted = TRUE THEN 'TRUE' ELSE 'FALSE' END as is_deleted
 , target_selectors -> 0 ->> 'research_identifier' as research_id
+, target_selectors -> 0 ->> 'userRole' as user_role
 , CASE WHEN LENGTH(text) > 1000
     THEN CONCAT(SUBSTRING(text for 1000), '[TRUNCATED]')
     ELSE text
   END as annotated_text
 , target_selectors -> 0 ->> 'content' AS highlighted_text
-, CASE WHEN target_selectors -> 0 ->> 'elementId' IS NOT NULL
+, CASE WHEN CAST(target_selectors -> 0 ->> 'elementId' as BOOLEAN) IS NOT NULL
     THEN target_selectors -> 0 ->> 'elementId'
     ELSE target_selectors -> 0 ->> 'referenceElementId'
   END as element_id
